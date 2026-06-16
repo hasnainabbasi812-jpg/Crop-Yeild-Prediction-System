968dd0ae8a5a59bc4700be076fd364893121772a
hasnainabbasi812-jpg <hasnainabbasi812@gmail.com>
2026-06-16 12:58:48 +0500

    Fresh initial commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.streamlit/
+echo "[theme]
+primaryColor='#00FF66'
+backgroundColor='#0A0A0A'
+secondaryBackgroundColor='#121212'
+textColor='#E0E0E0'
+font='sans serif'
+" > ~/.streamlit/config.toml
